c2f8f9cadec6f6f51bb32c37d0b545645422a604
grierJonah <jjg8895@gmail.com>
2021-03-11 19:53:32 -0800

    Dependencies, and package npm updates

--- set_game_project_2/src/index.js
+++ set_game_project_2/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Reducer from './Reducer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
+import Rules from './Rules/Rules.jsx'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path={"/"} component={App} />
+        <Route exact path={"/Rules/"} component={Rules} />
+        <Route exact path={"/Game/"} component={Rules} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
