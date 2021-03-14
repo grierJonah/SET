32b65cf336fd4961b811281d5779bc1d71c7b922
grierJonah <jjg8895@gmail.com>
2021-03-13 16:29:24 -0800

    Shape sizing properties

--- set_game_project_2/src/Game/Card/Card.jsx
+++ set_game_project_2/src/Game/Card/Card.jsx
@@ -19,19 +19,19 @@ export default class Card extends React.Component {
         console.log(this.state.shape);
         if (this.state.shape === "circle") {
             return (
-                <svg width="200" height="175">
+                <svg className="circle" width="200" height="175">
                     <circle cx="100" cy="60" r="50" strokeWidth="2" stroke="black" fill={this.state.color}></circle>
                 </svg>
             );
         } else if (this.state.shape === "square") {
             return (
-                <svg width="200" height="175" viewBox="0 0 100 100">
+                <svg className="square" width="200" height="175" viewBox="0 0 100 100">
                     <rect width="50" height="50" x="25" y="10" fill={this.state.color} stroke="black" strokeWidth="1" />
                 </svg>
             );
         } else {
             return (
-                <svg width="200" height="165" >
+                <svg className="triangle" width="200" height="165" >
                     <polygon points="100,10 50,100 150,100" fill={this.state.color} stroke="black" strokeWidth="2" />
                 </svg>
             );
