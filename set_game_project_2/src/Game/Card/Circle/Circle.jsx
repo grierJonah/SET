8dc256170935173b705bf04e23cecbdf3118e472
grierJonah <jjg8895@gmail.com>
2021-03-18 09:09:54 -0700

    SVG circle card renders multiple circles

--- set_game_project_2/src/Game/Card/Circle/Circle.jsx
+++ set_game_project_2/src/Game/Card/Circle/Circle.jsx
@@ -4,22 +4,37 @@ import React from 'react';
 export default class Circle extends React.Component {
 
     render() {
-        // for (let i = 0; i < this.props.num_shapes; i++) {
-        //     return (
-        //         <svg className="game-card circle" width="200" height="175" onClick={() => this.toggleCardColor("CHANGE_COLOR")}>
-        //             <circle cx="100" cy="65" r="40" strokeWidth="2" stroke="black" fill={this.props.color} ></circle>
-        //         </svg>
-        //     )
-        // }
-        return (
-            this.props.num_shapes.map((item, value) => {
-                console.log(item, value);
-                return (
-                    <svg className="game-card circle" width="200" height="175" key={value}>
-                        <circle cx="100" cy="65" r="40" strokeWidth="2" stroke="black" fill={item} ></circle>
-                    </svg>
-                );
-            })
-        );
+        const shapes = [];
+        const pattern = this.props.num_shapes[0]
+        console.log(this.props.num_shapes.length);
+        for (let i = 0; i < this.props.num_shapes.length; i++) {
+            shapes.push(<circle
+                cx={30 * i}
+                cy={-30 * i}
+                r="20"
+                fill={pattern}
+                stroke="black">
+            </circle>);
+        }
+
+        if (shapes.length == 1) {
+            return (
+                <svg className="game-card circle" width="200" height="200" viewBox="-50 -50 100 100">
+                    {shapes}
+                </svg>
+            );
+        } else if (shapes.length == 2) {
+            return (
+                <svg className="game-card circle" width="200" height="200" viewBox="-35 -65 100 100">
+                    {shapes}
+                </svg>
+            );
+        } else {
+            return (
+                <svg className="game-card circle" width="200" height="200" viewBox="-20 -80 100 100">
+                    {shapes}
+                </svg>
+            );
+        }
     }
 }
