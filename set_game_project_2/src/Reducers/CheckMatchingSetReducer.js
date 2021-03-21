030683e477dddd20d3391254b61cb811a1ec2744
grierJonah <jjg8895@gmail.com>
2021-03-21 12:01:30 -0700

    New game renders 3 matching pairs on start

--- set_game_project_2/src/Reducers/CheckMatchingSetReducer.js
+++ set_game_project_2/src/Reducers/CheckMatchingSetReducer.js
@@ -14,8 +14,6 @@ export default function (state = {
             if (card_one.color === card_two.color && card_one.color === card_three.color && card_two.color === card_three.color) {
                 if (card_one.pattern === card_two.pattern && card_one.pattern === card_three.pattern && card_two.pattern === card_three.pattern) {
                     console.log("SAME MATCHING SET!!!");
-
-
                     return {
                         matched_set_bool: true,
                         collected_sets: state.collected_sets.add({ card_one, card_two, card_three }),
@@ -27,7 +25,6 @@ export default function (state = {
             if (card_one.color !== card_two.color && card_one.color !== card_three.color && card_two.color !== card_three.color) {
                 if (card_one.pattern !== card_two.pattern && card_one.pattern !== card_three.pattern && card_two.pattern !== card_three.pattern) {
                     console.log("DIFF MATCHING SET!!!");
-
                     return {
                         matched_set_bool: true,
                         collected_sets: state.collected_sets.add({ card_one, card_two, card_three }),
@@ -36,11 +33,9 @@ export default function (state = {
                 }
             }
         } else {
-            console.log("YOU SUCK, NO MATCHING SET")
-
-            // Here we are using a boolean to dynamically set what our Card 'render' logic will do. 
-            // See line 57 in card. It pulls from this reducer to check if the 3-pair cards are a matching set or not. 
-            // If they are not (as we can see 'false') then the reset_selected_cards reducer is utilized
+            // Here we are using a boolean to dynamically set what our Card.jsx 'render' function logic will do. 
+            // See line 57 in Card.jsx. It pulls from this reducer to check if the 3-pair cards are a matching set or not. 
+            // If they are not a matching set (as we can see 'false') then the reset_selected_cards reducer is utilized
             return {
                 matched_set_bool: false,
             }
--- set_game_project_2/src/Reducers/NewGameReducer.js
+++ set_game_project_2/src/Reducers/NewGameReducer.js
@@ -19,13 +19,46 @@ export default function (state = {
         return array;
     }
 
+    function checkForSet(array) {
+        // return true or false, true will break, false will re-shuffle
+        for (let i = 0; i < 12 - 1; i++) {
+            for (let j = i + 1; j < 12 - 1; j++) {
+                for (let k = j + 1; k < 12 - 2; k++) {
+                    let new_arr = [array[i], array[j], array[k]];
+
+                    // Same Shape, Color, Pattern
+                    if (new_arr[i % 3].shape === new_arr[j % 3].shape && new_arr[i % 3].shape === new_arr[k % 3].shape && new_arr[j % 3].shape === new_arr[k % 3].shape) {
+                        if (new_arr[i % 3].color === new_arr[j % 3].color && new_arr[i % 3].color === new_arr[k % 3].color && new_arr[j % 3].color === new_arr[k % 3].color) {
+                            if (new_arr[i % 3].pattern === new_arr[j % 3].pattern && new_arr[i % 3].pattern === new_arr[k % 3].pattern && new_arr[j % 3].pattern === new_arr[k % 3].pattern) {
+                                if (new_arr[i % 3].number !== new_arr[j % 3].number && new_arr[i % 3].number !== new_arr[k % 3].number && new_arr[j % 3].number !== new_arr[k % 3].number) {
+                                    console.log("True", new_arr);
+                                    return true;
+                                }
+                            }
+                        }
+                        // Different Shape, Color, Pattern
+                    } else if (new_arr[i % 3].shape !== new_arr[j % 3].shape && new_arr[i % 3].shape !== new_arr[k % 3].shape && new_arr[j % 3].shape !== new_arr[k % 3].shape) {
+                        if (new_arr[i % 3].color !== new_arr[j % 3].color && new_arr[i % 3].color !== new_arr[k % 3].color && new_arr[j % 3].color !== new_arr[k % 3].color) {
+                            if (new_arr[i % 3].pattern !== new_arr[j % 3].pattern && new_arr[i % 3].pattern !== new_arr[k % 3].pattern && new_arr[j % 3].pattern !== new_arr[k % 3].pattern) {
+                                if (new_arr[i % 3].number !== new_arr[j % 3].number && new_arr[i % 3].number !== new_arr[k % 3].number && new_arr[j % 3].number !== new_arr[k % 3].number) {
+                                    console.log("True", new_arr);
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
     if (action.type === "NEW_GAME") {
         let index = 0;
-        for (const shape of shapes) {
+        for (const number of numbers) {
             for (const color of colors) {
                 for (const pattern of patterns) {
-                    for (const number of numbers) {
+                    for (const shape of shapes) {
                         deck.push({ index, number, color, pattern, shape })
                         index += 1
                     }
@@ -35,6 +68,19 @@ export default function (state = {
 
         // randomize ordering of the svg objects
         let shuffled_deck = shuffle(deck);
+
+        // Continuously Re-shuffle if first 12 cards do not contain a SET
+        let flag = true;
+        while (flag) {
+            if (checkForSet(shuffled_deck)) {
+                console.log("Found set!", shuffled_deck);
+                flag = false;
+                break;
+            }
+            console.log("Reshuffling");
+            shuffled_deck = shuffle(shuffled_deck);
+        }
+
         let starting_board = [];
         // let picked_cards = [];
 
@@ -48,7 +94,6 @@ export default function (state = {
         for (let i = 0; i < 12; i++) {
             shuffled_deck.shift();
         }
-        console.log("Current Deck", state.current_deck);
         return {
             ...state,
             current_deck: shuffled_deck,
