0e7b45292f14efdf881bba78128be17cabab3f84
grierJonah <jjg8895@gmail.com>
2021-03-23 14:45:47 -0700

    Added spacing code to rules

--- set_game_project_2/src/Rules/Rules.jsx
+++ set_game_project_2/src/Rules/Rules.jsx
@@ -10,12 +10,12 @@ export default class Rules extends React.Component {
                 <p className="rules-explanation">SET is a card game that contains 81 cards. Each card has 4 different features, with 3
                 variations on each feature:</p>
                 <ul>
-                    <li><b>Variation 1:</b> Shape on card (e.g., square, circle, triangle)</li>
-                    <li><b>Variation 2:</b> Color of shape (e.g., green, blue, or red)</li>
-                    <li><b>Variation 3:</b> Number of shapes (1, 2 or 3)</li>
-                    <li><b>Variation 4:</b> Shading of shape (none, striped, solid)</li>
+                    <li><b>Variation 1:&nbsp;</b> Shape on card (e.g., square, circle, triangle)</li>
+                    <li><b>Variation 2:&nbsp;</b> Color of shape (e.g., green, blue, or red)</li>
+                    <li><b>Variation 3:&nbsp;</b> Number of shapes (1, 2 or 3)</li>
+                    <li><b>Variation 4:&nbsp;</b> Shading of shape (none, striped, solid)</li>
                 </ul>
-                <p className="rules-explanation second">Select 3 cards that have <u>all</u> or <u>none</u> of the same variations</p>
+                <p className="rules-explanation second">Select 3 cards that have <u>all</u> or <u>none</u> of the same variations (excluding the number)</p>
                 <div class="back-button">
                     <form action="../">
                         <button className="rules-game-button" type="submit" id="back">Back</button>
